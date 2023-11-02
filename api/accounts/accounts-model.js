406f05971795bf44ef49e41da878127e25f77c12
DJ1 <jimmynoble604@gmail.com>
2023-11-02 12:23:57 -0400

    account-module.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,24 @@
 /* eslint-disable no-unused-vars */
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('accounts');
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+
 }
 
 module.exports = {
